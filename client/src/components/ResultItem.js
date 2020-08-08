6fd68df5f46ebc90150ae155150436f45f131b90
Susan Holland <segh@fastmail.com>
2020-08-08 19:37:26 -0400

    Made onClick function to console.log bookdata

--- client/src/components/ResultItem.js
+++ client/src/components/ResultItem.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "materialize-css";
 import { CollectionItem, Button, Row, Col } from "react-materialize";
+import API from "../utils/API";
 
 export default function ResultItem(props) {
+    function saveBook() {
+        console.log(props);
+    }
+
     return (
         <CollectionItem>
 
             <Row>
                 <Col><h6>{props.title}</h6><p>{props.authors}</p></Col>
                 <Col><a href={props.link} className="btn">View</a></Col>
-                <Col><Button>Save</Button></Col>
+                <Col><Button onClick={ saveBook }>Save</Button></Col>
             </Row>
             <Row>
                 <Col><img src={props.image}></img></Col>
