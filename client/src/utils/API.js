56fce858c36e13596af3876794414b1b37083872
Susan Holland <segh@fastmail.com>
2020-08-08 19:36:26 -0400

    saveBook method

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,8 @@ import axios from "axios";
 export default {
     getBooks: function(query) {
         return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query + "&key=" + "AIzaSyBsEW3rt6CfUI4K7qv6CBX4Lz3ovbjZFw4");
+    },
+    saveBook: function(bookData) {
+        return axios.post("/api/books", bookData);
     }
 }
