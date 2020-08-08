a66bf4231c447b5fafb822481f28a1507241545b
Susan Holland <segh@fastmail.com>
2020-08-08 17:05:00 -0400

    Added Results componenent

---
+++ client/src/components/Results.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "materialize-css";
+import { Collection, CollectionItem } from "react-materialize";
+
+export default function Results() {
+    return (
+        <section>
+            <h6>Results</h6>
+        </section>
+    );
+}
--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Searchbar from "../Searchbar";
+import Results from "../Results";
 
 export default function Search() {
     return (
         <main className="container">
             <Searchbar />
+            <Results />
         </main>
     );
 }
