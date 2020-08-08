e462a17de1afca29c82b26835f52f30757f80047
Susan Holland <segh@fastmail.com>
2020-08-08 15:11:37 -0400

    Methods for querying database

---
+++ controllers/booksControllers.js
@@ -0,0 +1,25 @@
+const db = require("../models");
+
+// Methods for querying the database
+module.exports = {
+    findAll: function (req, res) {
+        db.Book
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(books => res.json(books))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function (req, res) {
+        db.Book
+            .create(req.body)
+            .then(book => res.json(book))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+        db.Book
+            .findById({ _id: req.params.id })
+            .then(book => book.remove())
+            .then(book => res.json(book))
+            .catch(err => res.status(422).json(err));
+    }
+};
