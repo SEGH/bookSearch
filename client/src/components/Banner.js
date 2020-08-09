0459fbe602d2bb4c1437cbafe06abc5615459c99
Susan Holland <segh@fastmail.com>
2020-08-09 14:30:48 -0400

    Added Banner component

---
+++ client/src/components/Banner.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Banner() {
+    return (
+        <section id="banner" className="center z-depth-2">
+            <h2 className="hide-on-small-only">(React) Google Books Search</h2>
+            <h5 className="flow-text">Search for and Save Books of Interest</h5>
+        </section>
+    );
+}
