65c11cfaafbb8d032da8cdf9f369880507f47332
Susan Holland <segh@fastmail.com>
2020-08-09 14:32:56 -0400

    Added depth to collection

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -5,7 +5,7 @@ import ResultItem from "./ResultItem";
 
 export default function Results(props) {
     return (
-        <section>
+        <section className="z-depth-1">
             <h6>Results</h6>
             <Collection>
                 {props.books.map(book => <ResultItem title={book.volumeInfo.title} authors={book.volumeInfo.authors.join(", ")} image={book.volumeInfo.imageLinks.smallThumbnail} description={book.volumeInfo.description} link={book.volumeInfo.infoLink} />)}
