9f3bb1613541903fd90473763396ed0c3b632806
Susan Holland <segh@fastmail.com>
2020-08-09 14:52:00 -0400

    Added condition to map for user in both saved and search

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import "materialize-css";
 import { Collection } from "react-materialize";
 import ResultItem from "./ResultItem";
 
 export default function Results(props) {
+    const location = useLocation();
+
     return (
         <section className="z-depth-1">
             <h6>Results</h6>
             <Collection>
-                {props.books.map(book => <ResultItem title={book.volumeInfo.title} authors={book.volumeInfo.authors.join(", ")} image={book.volumeInfo.imageLinks.smallThumbnail} description={book.volumeInfo.description} link={book.volumeInfo.infoLink} />)}
+                {props.books.map(book => {
+                    if (location.pathname === "/") {
+                        book = book.volumeInfo;
+                    }
+                    return <ResultItem title={book.title} authors={book.authors.join(", ")} image={book.imageLinks ? book.imageLinks.smallThumbnail : ""} description={book.description} link={book.infoLink} />
+                }
+                )}
             </Collection>
         </section>
     );
