4702f60749214bbe66b791bd083ea0d982db03c8
Susan Holland <segh@fastmail.com>
2020-08-09 15:19:50 -0400

    Added condition to ResultItem to use save button on search page and delete button on saved page

--- client/src/components/ResultItem.js
+++ client/src/components/ResultItem.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import "materialize-css";
 import { CollectionItem, Button, Row, Col } from "react-materialize";
 import API from "../utils/API";
 
 export default function ResultItem(props) {
+    const location = useLocation();
+
     function saveBook() {
         console.log(props);
         API.saveBook(props)
@@ -13,13 +16,19 @@ export default function ResultItem(props) {
             .catch(err => console.log(err));
     };
 
+    function deleteBook() {
+        console.log(props);
+    }
+
     return (
         <CollectionItem>
 
             <Row>
                 <Col l={10}><h6>{props.title}</h6><p>{props.authors}</p></Col>
                 <Col l={1}><a href={props.link} className="btn-small z-depth-2 amber">View</a></Col>
-                <Col l={1}><Button small className="z-depth-2 amber" onClick={ saveBook }>Save</Button></Col>
+                <Col l={1}>
+                    {location.pathname === "/" ? <Button small className="z-depth-2 amber" onClick={ saveBook }>Save</Button> : <Button small className="z-depth-2 red" onClick={ deleteBook }>Delete</Button>}
+                    </Col>
             </Row>
             <Row>
                 <Col l={2}><img src={props.image}></img></Col>
