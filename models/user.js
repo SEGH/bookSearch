57c832b0ef3f75aabdea82806566c7dfe6e602d3
Susan Holland <segh@fastmail.com>
2020-09-19 13:43:09 -0400

    Created user schema

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema ({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    books: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Book"
+        }
+    ]
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
