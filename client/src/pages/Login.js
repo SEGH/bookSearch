dca75fe4b2ffd945940852478f7c9d9f246c10eb
Susan Holland <segh@fastmail.com>
2020-09-18 21:32:55 -0400

    Added LoginInput component

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "materialize-css";
 import Banner from "../components/Banner";
+import LoginInput from "../components/LoginInput";
 
 export default function Login() {
     return (
         <main className="container">
             <Banner />
-            
+            <LoginInput />
         </main>
     )
 }
