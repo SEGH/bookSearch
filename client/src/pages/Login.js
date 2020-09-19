a567379ac8a23ea7ccfe0c14fa1a8dfb68119725
Susan Holland <segh@fastmail.com>
2020-09-18 20:50:08 -0400

    Created Login page with banner

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "materialize-css";
+import Banner from "../components/Banner";
+
+export default function Login() {
+    return (
+        <main className="container">
+            <Banner />
+            
+        </main>
+    )
+}
