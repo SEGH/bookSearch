94960256f6b374436d071a09c358506c64a921a0
Susan Holland <segh@fastmail.com>
2020-09-18 22:16:21 -0400

    Added button, passed prop to toggle form

--- client/src/components/LoginInput.js
+++ client/src/components/LoginInput.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "materialize-css";
-import { TextInput } from "react-materialize";
+import { TextInput, Button } from "react-materialize";
 
-export default function LoginInput() {
+export default function LoginInput(props) {
     return (
-        <section id="login" className="center z-depth-1">
-            <h4>Login</h4>
+        <section className="center z-depth-1 login">
             <TextInput email label="Email" placeholder="name@test.com" icon="account_circle" error="Something's not right..." success="Looks good!" validate />
             <TextInput password label="Password" placeholder="password1234" icon="vpn_key" error="Something's not right..." success="Looks good!" validate />
+            <Button large waves="yellow" className="z-depth-2 amber">Login</Button>
+            <p onClick={props.toggleForm}>Or Sign Up</p>
         </section>
     )
 }
