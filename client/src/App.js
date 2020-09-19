744fc7edf59c5a81b86153a840387380b69c97a6
Susan Holland <segh@fastmail.com>
2020-09-18 20:50:47 -0400

    Added route to Login page, updated route to search page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Login from "./pages/Login";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 
@@ -8,7 +9,8 @@ export default function App() {
     return (
         <Router>
             <Header />
-            <Route exact path="/" component={Search} />
+            <Route exact path="/" component={Login} />
+            <Route exact path="/search" component={Search} />
             <Route exact path="/saved" component={Saved} />
         </Router>
     );
