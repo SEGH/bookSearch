104cc0a934fdde242342b26348c704345721e99a
Susan Holland <segh@fastmail.com>
2020-09-19 12:40:21 -0400

    Wrapped text inputs in form

--- client/src/components/LoginInput.js
+++ client/src/components/LoginInput.js
@@ -5,10 +5,12 @@ import { TextInput, Button } from "react-materialize";
 export default function LoginInput(props) {
     return (
         <section className="center z-depth-1 login">
-            <TextInput email label="Email" placeholder="name@test.com" icon="account_circle" error="Something's not right..." success="Looks good!" validate />
-            <TextInput password label="Password" placeholder="password1234" icon="vpn_key" error="Something's not right..." success="Looks good!" validate />
-            <Button large waves="yellow" className="z-depth-2 amber">Login</Button>
-            <p onClick={props.toggleForm}>Or Sign Up</p>
+            <form action="/login" method="post">
+                <TextInput email label="Email" placeholder="name@test.com" icon="account_circle" error="Something's not right..." success="Looks good!" validate />
+                <TextInput password label="Password" placeholder="password1234" icon="vpn_key" error="Something's not right..." success="Looks good!" validate />
+                <Button type="submit" large waves="yellow" className="z-depth-2 amber">Login</Button>
+                <p onClick={props.toggleForm}>Or Sign Up</p>
+            </form>
         </section>
     )
 }
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -5,10 +5,12 @@ import { TextInput, Button } from "react-materialize";
 export default function SignUp(props) {
     return (
         <section id="signup" className="center z-depth-1 login">
-            <TextInput email label="Email" placeholder="name@test.com" icon="account_circle" error="Something's not right..." success="Looks good!" validate />
-            <TextInput password label="Password" placeholder="password1234" icon="vpn_key" error="Something's not right..." success="Looks good!" validate />
-            <Button large waves="yellow" className="amber z-depth-2">Sign Up</Button>
-            <p onClick={props.toggleForm}> Or Login</p>
+            <form action="/signup" method="post">
+                <TextInput email label="Email" placeholder="name@test.com" icon="account_circle" error="Something's not right..." success="Looks good!" validate />
+                <TextInput password label="Password" placeholder="password1234" icon="vpn_key" error="Something's not right..." success="Looks good!" validate />
+                <Button type="submit" large waves="yellow" className="amber z-depth-2">Sign Up</Button>
+                <p onClick={props.toggleForm}> Or Login</p>
+            </form>
         </section>
     )
 }
