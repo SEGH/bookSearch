ca204150081db98a58bea13552cbca5365215553
Susan Holland <segh@fastmail.com>
2020-09-19 12:56:33 -0400

    Added passport

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const passport = require("passport");
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(passport.initialize());
 
 // Serving static assets
 if (process.env.NODE_ENV === "production") {
