b054dcf0950b0e4782f1e528e17fae094ddaab40
Susan Holland <segh@fastmail.com>
2020-09-18 22:18:43 -0400

    Added user hook and condition to display login or signup form if user or not

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "materialize-css";
 import Banner from "../components/Banner";
 import LoginInput from "../components/LoginInput";
+import SignUp from "../components/SignUp";
 
 export default function Login() {
+    const [user, setUser] = useState(true);
+    const toggleForm = () => setUser(!user);
+
     return (
         <main className="container">
             <Banner />
-            <LoginInput />
+            {user ? <LoginInput toggleForm={toggleForm} /> : <SignUp toggleForm={toggleForm} />}
         </main>
     )
 }
