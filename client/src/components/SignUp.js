2331ba9b1f72b5f891c4afbcfcc6312026c5770f
Susan Holland <segh@fastmail.com>
2020-09-19 21:22:31 -0400

    Added signupform hook and handleinput and submitform for API call to create user

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "materialize-css";
 import { TextInput, Button } from "react-materialize";
+import API from "../utils/API";
 
 export default function SignUp(props) {
+    const [signUpForm, setSignUpForm] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleInputChange = event => {
+        const { value } = event.target;
+        const { name } = event.target;
+
+        setSignUpForm({
+            ...signUpForm,
+            [name]: value
+        });
+    }
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+
+        API.createUser(signUpForm)
+            .then(res => {
+                console.log("User created")
+            })
+            .catch(err => console.log(err));
+    }
+
     return (
         <section id="signup" className="center z-depth-1 login">
-            <form action="/signup" method="post">
-                <TextInput email label="Email" placeholder="name@test.com" icon="account_circle" error="Something's not right..." success="Looks good!" validate />
-                <TextInput password label="Password" placeholder="password1234" icon="vpn_key" error="Something's not right..." success="Looks good!" validate />
+            <form action="/" onSubmit={handleFormSubmit} method="post">
+                <TextInput onChange={handleInputChange} email label="Email" placeholder="name@test.com" icon="account_circle" error="Something's not right..." success="Looks good!" name="email" />
+                <TextInput onChange={handleInputChange} password label="Password" placeholder="password1234" icon="vpn_key" error="Something's not right..." success="Looks good!" name="password" />
                 <Button type="submit" large waves="yellow" className="amber z-depth-2">Sign Up</Button>
                 <p onClick={props.toggleForm}> Or Login</p>
             </form>
