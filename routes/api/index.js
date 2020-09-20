851ef9ea4dd9868bdeb006cc1f9d87836a2438d4
Susan Holland <segh@fastmail.com>
2020-09-19 21:19:44 -0400

    Added userRoutes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
+const userRoutes = require("./users");
 
 router.use("/books", bookRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
