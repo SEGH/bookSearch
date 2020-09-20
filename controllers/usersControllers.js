a6ab6326245add8605cb25cee953189f1db460cc
Susan Holland <segh@fastmail.com>
2020-09-19 21:32:58 -0400

    Used bcrypt to hash password when creating user

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,9 +1,14 @@
 const db = require("../models");
+const bcrypt = require("bcrypt");
 
 module.exports = {
     create: function (req, res) {
+        const hash = bcrypt.hashSync(req.body.password, 12);
         db.User
-            .create(req.body)
+            .create({
+                email: req.body.email,
+                password: hash
+            })
             .then(user => res.json(user))
             .catch(err => res.status(422).json(err));
     }
