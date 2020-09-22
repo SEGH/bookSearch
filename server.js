6111bb400153280bef4ca7399639b394fb38a540
Susan Holland <segh@fastmail.com>
2020-09-21 20:23:30 -0400

    Used middleware for authentication

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const passport = require("passport");
+const passport = require("./config/passport");
+const session = require("express-session");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
+app.use(passport.session());
 
 // Serving static assets
 if (process.env.NODE_ENV === "production") {
@@ -21,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useUnifiedTopology: true, useNewUrlParser: true });
 
 // Start the API server
 app.listen(PORT, function() {
