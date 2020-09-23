0e5d551b37bb40e5753c89f7f660439739ac2d6f
Susan Holland <segh@fastmail.com>
2020-09-23 12:58:34 -0400

    Added passport local strategy

---
+++ config/passport.js
@@ -0,0 +1,35 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const db = require("../models");
+
+// Authentication strategy to use email and password for login
+passport.use(new LocalStrategy({
+    // Passport expects username and password, so this lets it know we are using email instead
+    usernameField: "email"
+}, function (username, password, done) {
+
+    db.User.findOne({ email: username }, function (err, user) {
+        if (err) { return done(err); }
+
+        if (!user) {
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+
+        if (!user.validPassword(password)) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+
+        return done(null, user);
+    });
+}
+));
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+module.exports = passport;
