9e988cf1735a4e8274dd609f97f23e420fdcc8f6
Susan Holland <segh@fastmail.com>
2020-10-03 18:46:39 -0400

    Passed loggedIn prop to Search and Saved

--- client/src/App.js
+++ client/src/App.js
@@ -86,8 +86,8 @@ export default function App() {
         <Router>
             <Header loggedIn={loggedIn} logoutUser={logoutUser} />
             <Route exact path="/login" render={(props) => ( <Login {...props} handleLoginSubmit={handleLoginSubmit} handleLoginInput={handleLoginInput} handleSignupInput={handleSignupInput} handleSignupSubmit={handleSignupSubmit} loggedIn={loggedIn} user={user} />)} />
-            <Route exact path="/" render={(props) => ( <Search {...props} userId={user.id} />)} />
-            <Route exact path="/saved" render={(props) => ( <Saved {...props} userId={user.id} />)} />
+            <Route exact path="/" render={(props) => ( <Search {...props} userId={user.id} loggedIn={loggedIn} />)} />
+            <Route exact path="/saved" render={(props) => ( <Saved {...props} userId={user.id} loggedIn={loggedIn} />)} />
         </Router>
     );
 }
