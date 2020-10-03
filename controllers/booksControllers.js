811f0f0758af961772ee610f5a6cef87fc88931f
Susan Holland <segh@fastmail.com>
2020-10-03 14:38:59 -0400

    Added querying for User model in findAll populating books and in create method pushing book to User

--- controllers/booksControllers.js
+++ controllers/booksControllers.js
@@ -1,21 +1,42 @@
 const db = require("../models");
+const mongoose = require("mongoose");
 
 // Methods for querying the database
 module.exports = {
     findAll: function (req, res) {
-        db.Book
-            .find({})
-            .sort({ date: -1 })
+        db.User
+            .findById({ _id: req.params.userId })
+            .populate({
+                path: "books"
+            })
+            // .sort({ date: -1 })
             .then(books => res.json(books))
             .catch(err => res.status(422).json(err));
     },
     create: function (req, res) {
+        console.log(req.body);
         db.Book
-            .create(req.body)
-            .then(book => res.json(book))
+            .create({
+                title: req.body.title,
+                authors: req.body.authors,
+                description: req.body.description,
+                image: req.body.image,
+                link: req.body.link
+            })
+            .then(book => {
+                console.log(book);
+                db.User.findOneAndUpdate({ _id: mongoose.Types.ObjectId(req.params.userId) }, { $push: { books: book } })
+                    .then(user => {
+                        console.log(user);
+                        res.json(user);
+                    })
+                    .catch(err => {
+                        res.status(422).json(err);
+                    });
+            })
             .catch(err => res.status(422).json(err));
     },
-    remove: function(req, res) {
+    remove: function (req, res) {
         db.Book
             .findById({ _id: req.params.id })
             .then(book => book.remove())
