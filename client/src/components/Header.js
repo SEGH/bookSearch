24eecff1f83c24915ad07fb0aa4b65632a20dfb1
Susan Holland <segh@fastmail.com>
2020-10-03 14:20:40 -0400

    Added Login and Logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "materialize-css";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Navbar } from "react-materialize";
 
 export default function Header(props) {
-    const location = useLocation();
 
     return (
         <Navbar brand={<a className="brand-logo" href="/">Google Books</a>} alignLinks="left" centerLogo className="deep-orange lighten-3">
@@ -13,6 +12,7 @@ export default function Header(props) {
 
             {props.loggedIn ? <Link to="/saved">Saved</Link> : null}
 
+            {props.loggedIn ? <Link to= "/login">Logout</Link> : <Link to= "/login">Login</Link>}
         </Navbar>
     )
 }
