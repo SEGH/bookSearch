9507a974b89025baa12f51a6bf27dc1cfd1f3c8e
Susan Holland <segh@fastmail.com>
2020-10-03 14:40:21 -0400

    Added userFindAndModify: false to mongoose.connect

--- server.js
+++ server.js
@@ -11,10 +11,13 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
-
+app.use( (req, res, next) => {
+  console.log('req.session', req.session);
+  return next();
+});
 // Serving static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -24,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
 
 // Start the API server
 app.listen(PORT, function() {
