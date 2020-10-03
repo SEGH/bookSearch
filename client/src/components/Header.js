0c63c09c7b8b5d2ea0fbec1166b65a11d2b3e40c
Susan Holland <segh@fastmail.com>
2020-10-03 18:29:12 -0400

    Made brand router Link and changed Link order

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,13 +6,12 @@ import { Navbar } from "react-materialize";
 export default function Header(props) {
 
     return (
-        <Navbar brand={<a className="brand-logo" href="/">Google Books</a>} alignLinks="left" centerLogo className="deep-orange lighten-3">
+        <Navbar brand={<Link className="brand-logo" to="/">Google Books</Link>} alignLinks="left" centerLogo className="deep-orange lighten-3">
 
+            {props.loggedIn ? <Link to="/login" onClick={props.logoutUser}>Logout</Link> : <Link to="/login">Login</Link>}
             <Link to="/">Search</Link>
-
             {props.loggedIn ? <Link to="/saved">Saved</Link> : null}
 
-            {props.loggedIn ? <Link to= "/login">Logout</Link> : <Link to= "/login">Login</Link>}
         </Navbar>
     )
 }
