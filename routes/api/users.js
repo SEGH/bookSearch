e074abfdbab8fa3096d45840a75a114bb4268bbf
Susan Holland <segh@fastmail.com>
2020-11-01 14:28:19 -0500

    Added route to get logged in user

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,12 +2,16 @@ const router = require("express").Router();
 const usersController = require("../../controllers/usersControllers");
 const passport = require("../../config/passport");
 
+router.route("/")
+    .get(usersController.getUser);
+
 router.route("/register")
     .post(usersController.create);
 
 router.route("/login")
     .post(passport.authenticate("local"), (req, res) => {
         // This function will run if authentication is successful and send back the user's email and id
+        console.log(req.user.email);
         res.json({
             email: req.user.email,
             id: req.user.id
