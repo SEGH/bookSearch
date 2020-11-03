6e83b17259f77c63715f5317d1b51677fbac08e3
Susan Holland <segh@fastmail.com>
2020-11-03 16:35:49 -0500

    Reordered middleware and removed session console.log

--- server.js
+++ server.js
@@ -11,25 +11,23 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
-}));
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
-app.use(cookieParser(process.env.SESSION_SECRET));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use( (req, res, next) => {
-  console.log('req.session', req.session);
-  return next();
-});
 // Serving static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Middleware
+app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({
+  origin: "http://localhost:3001",
+  credentials: true
+}));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 app.use(routes);
