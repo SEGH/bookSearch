17426640bec24eb081e1d599da1c0d421f0b05b4
Susan Holland <segh@fastmail.com>
2020-11-04 16:23:59 -0500

    Added booksLength state and made useEffect dependency

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect } from "react";
 import "materialize-css";
 import { Icon } from "react-materialize";
 import Results from "../components/Results";
@@ -7,11 +7,12 @@ import API from "../utils/API";
 
 export default function Saved(props) {
     const [books, setBooks] = useState([]);
+    const [booksLength, setBooksLength] = useState([books.length]);
 
     useEffect(() => {
         function loadBooks() {
-            console.log(props.userId);
-            API.getSavedBooks(props.userId)
+            console.log(props.user);
+            API.getSavedBooks(props.user.id)
                 .then(res => {
                     console.log(res.data.books);
                     setBooks(res.data.books);
@@ -21,12 +22,12 @@ export default function Saved(props) {
         };
 
         loadBooks();
-    }, [props.userId]);
+    }, [props.user, booksLength]);
 
     return (
         <main className="container">
             <Banner />
-            {books.length > 0 ? <Results books={books} loggedIn={props.loggedIn} /> : <section className="bookDiv"><Icon large >local_library</Icon><h4>No Saved Books</h4></section>}
+            {books.length > 0 ? <Results books={books} booksLength={booksLength} setBooksLength={setBooksLength} loggedIn={props.loggedIn} /> : <section className="bookDiv"><Icon large >local_library</Icon><h4>No Saved Books</h4></section>}
         </main>
     );
 }
