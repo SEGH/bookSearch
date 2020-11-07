3320437e852e0ca856850aabff0f89879dbe5710
Susan Holland <segh@fastmail.com>
2020-11-07 15:07:14 -0500

    Added html5 form validation

--- client/src/components/LoginInput.js
+++ client/src/components/LoginInput.js
@@ -6,8 +6,8 @@ export default function LoginInput(props) {
     return (
         <section className="center z-depth-1 login">
             <form method="post" onSubmit={props.handleLoginSubmit}>
-                <TextInput id="loginEmail" onChange={props.handleLoginInput} email label="Email" placeholder="name@test.com" icon="account_circle" error="Something's not right..." success="Looks good!" validate name="email" />
-                <TextInput id="loginPassword" onChange={props.handleLoginInput} password label="Password" placeholder="password1234" icon="vpn_key" error="Something's not right..." success="Looks good!" validate name="password" />
+                <TextInput id="loginEmail" onChange={props.handleLoginInput} email required maxLength={100} label="Email" placeholder="name@test.com" icon="account_circle" error="Something's not right..." success="Looks good!" validate name="email" />
+                <TextInput id="loginPassword" onChange={props.handleLoginInput} password required minLength={6} maxLength={20} label="Password" placeholder="password1234" icon="vpn_key" error="Something's not right..." success="Looks good!" validate name="password" />
                 <Button type="submit" large waves="yellow" className="z-depth-2 amber">Login</Button>
                 <p onClick={props.toggleForm}>Or Sign Up</p>
             </form>
