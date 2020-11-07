652fe64ffdbaf61df50bddec68431a6fbd1f6d00
Susan Holland <segh@fastmail.com>
2020-11-07 15:07:37 -0500

    Added html5 input validation

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -6,7 +6,7 @@ export default function Searchbar(props) {
     return (
         <section className="z-depth-1">
             <h6>Book Search</h6>
-            <TextInput placeholder="Book" onChange={props.handleInputChange} value={props.bookSearch} />
+            <TextInput maxLength="100" placeholder="Book" onChange={props.handleInputChange} value={props.bookSearch} />
             <Button node="button" small className="z-depth-2 amber" onClick={props.handleSearch}>Search</Button>
         </section>
     );
